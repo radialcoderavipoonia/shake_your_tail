6f7fdae6f4084f3c50021a882e9fa478147a6d4e
sukhvinder-jaat <monudhaka829@gmail.com>
2023-09-19 14:52:29 +0530

    hero section complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
-import MyNav from "./components/MyNav";
 import Features from "./components/Features";
-
+import { Hero } from "./components/Hero";
+import yellowCircle from './assets/images/svg/heroYellowCircle.svg'
+import whiteCircle from "./assets/images/svg/heroWhiteCircle.svg";
 function App() {
   return (
     <>
-      <div className="bg-[#fff]">
-        <MyNav />
-        <Features/>
+      <div className="bg-[#fff] overflow-hidden">
+        <div className="relative bgHero bg-no-repeat lg:bg-transparent bg-[#FFC100]">
+          <Hero />
+          <div>
+            <img src={ yellowCircle} className="end-0 absolute top-[13%] md:h-[288px] h-[140px]" />
+            <img src={ whiteCircle} className="start-0 absolute md:bottom-[33%] bottom-10 h-[140px] sm:block hidden" />
+          </div>
+        </div>
+        <Features />
       </div>
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import MyNav from "../components/MyNav";
+import mobile from "../assets/images/png/heroMobile.png";
+export const Hero = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center">
+      <MyNav />
+      <div className="container px-3 lg:px-16 mx-auto mt-8 pb-10 flex-1">
+        <div className="flex flex-wrap items-center lg:flex-row flex-col-reverse">
+          <div className="lg:w-1/2 w-full flex flex-col lg:items-start items-center lg:pb-20">
+            <p className=" text-[80px] lg:text-start text-center font-roboto font-bold text-white leading-[101%]">
+              Smart, Simple Software
+            </p>
+            <p className="font-helveticaMedium lg:text-start lg:pt-0 pt-3 text-center font-medium text-xl text-white max-w-[510px] lg:max-w-[490px]">
+              Dog Grooming Software, helping to reduce 'no shows' with
+              appointment confirmations, reminders & more.
+            </p>
+            <div className="flex items-center mt-[33px] lg:text-start text-center">
+              <button className="font-roboto flex items-start justify-center text-xl py-[14px] px-[30px] bg-[#5B95E0] rounded-full text-white me-3">
+                Free Trial
+              </button>
+              <button className="font-roboto flex items-start justify-center text-xl py-[14px] px-[30px] bg-[#FFC100] rounded-full text-white shadow-[0px_4px_25px_0px_#FFC100] border border-white">
+                Explore More
+              </button>
+            </div>
+          </div>
+          <div className="lg:w-1/2 w-full flex items-center justify-center">
+            <img src={mobile} alt="mobile" className="lg:w-full w-[60%]" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
