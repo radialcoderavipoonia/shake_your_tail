438b2f99759fbe33398c7f2739ceb04a74eefaed
sukhvinder-jaat <monudhaka829@gmail.com>
2023-09-21 11:49:30 +0530

    new

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -14,14 +14,14 @@ const BackToTop = () => {
   });
   return (
     <section>
-      <div className=" container">
+      <div className=" container relative z-10">
         <div className="z-10 h-[20px] w-[20px]">
           <button
             onClick={() => onTop()}
             className={
               first
-                ? "hidden text-white me-3 hover:shadow-[0px_4px_25px_0px_#FFC100] hover:bg-[#FFC100] hover:border hover:border-white border border-transparent py-[14px] px-[30px] bg-[#5B95E0] rounded-full font-helveticaRegular font-bold text-xl end-[40px] bottom-[40px] fixed"
-                : "block text-white me-3 hover:shadow-[0px_4px_25px_0px_#FFC100] hover:bg-[#FFC100] hover:border hover:border-white border border-transparent py-[14px] px-[30px] bg-[#5B95E0] rounded-full font-helveticaRegular font-bold text-xl end-[40px] bottom-[40px] fixed"
+                ? "hidden text-white me-3 hover:shadow-[0px_4px_25px_0px_#FFC100] hover:bg-[#FFC100] hover:border hover:border-white border border-transparent py-[14px] px-[30px] bg-[#5B95E0] rounded-full font-helveticaRegular font-bold text-xl sm:end-[40px] end-1 sm:bottom-[40px] bottom-1 fixed"
+                : "block text-white me-3 hover:shadow-[0px_4px_25px_0px_#FFC100] hover:bg-[#FFC100] hover:border hover:border-white border border-transparent py-[14px] px-[30px] bg-[#5B95E0] rounded-full font-helveticaRegular font-bold text-xl sm:end-[40px] end-1 sm:bottom-[40px] bottom-1 fixed"
             }
           >
             Top
